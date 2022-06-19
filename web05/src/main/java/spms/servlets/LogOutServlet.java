d6dc2944172041e0ecf3072be51854641964ccd5
user <stbhg5@gmail.com>
2022-06-19 20:33:10 +0900

    2022.06.19 교재실습

---
+++ web05/src/main/java/spms/servlets/LogOutServlet.java
@@ -0,0 +1,23 @@
+package spms.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/auth/logout")
+public class LogOutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		//세션 객체 무효화(객체 제거)
+		session.invalidate();
+		response.sendRedirect("login");
+	}
+}
--- web05/src/main/webapp/Header.jsp
+++ web05/src/main/webapp/Header.jsp
@@ -1,5 +1,13 @@
-<%@page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import="spms.vo.Member"%>
+<%
+//세션 객체를 가져와 member 객체로 넣음
+Member member = (Member)session.getAttribute("member");
+%>
 <div style="background-color:#00008b; color:#ffffff; height:20px; padding:5px;">
-SPMS(Simple Project Management System)
+	SPMS(Simple Project Management System)
+	<span style="float:right;">
+		<%=member.getName()%>
+		<a style="color:white;" href="<%=request.getContextPath()%>/auth/logout">로그아웃</a>
+	</span>
 </div>
