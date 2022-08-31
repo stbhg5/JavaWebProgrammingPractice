12e2bb246285d05dc89129ff1fe4ce2d7a0c0209
stbhg5 <stbhg5@gmail.com>
2022-08-31 23:34:44 +0900

    2022.08.31 교재실습

--- web07/src/main/java/spms/listeners/ContextLoaderListener.java
+++ web07/src/main/java/spms/listeners/ContextLoaderListener.java
@@ -1,15 +1,19 @@
 package spms.listeners;
 
+import java.io.InputStream;
+
 import javax.naming.InitialContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-//import javax.servlet.annotation.WebListener;
 
 import javax.servlet.annotation.WebListener;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import spms.context.ApplicationContext;
 import spms.controls.LogInController;
@@ -20,7 +24,7 @@ import spms.controls.MemberListController;
 import spms.controls.MemberUpdateController;
 import spms.dao.MySqlMemberDao;
 
-//프로퍼티 파일 적용 : ApplicationContext 사용
+//SqlSessionFactory 객체 준비
 @WebListener
 public class ContextLoaderListener implements ServletContextListener {
 	static ApplicationContext applicationContext;
@@ -33,12 +37,30 @@ public class ContextLoaderListener implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
 		try {
+			//기본 생성자 호출
+			applicationContext = new ApplicationContext();
+			
+			//빌더 패턴(Builder Pattern) 객체 생성
+			String resource = "spms/dao/mybatis-config.xml";
+			//getResourceAsStream() : 자바 클래스 경로(CLASSPATH)에 있는 파일의 입력 스트림 얻음
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			
+			//ApplicationContext 클래스에 SqlSessionFactory 객체 등록
+			applicationContext.addBean("sqlSessionFactory", sqlSessionFactory);
+			
 			//DB 커넥션 객체 준비
 			ServletContext sc = event.getServletContext();
 			
 			//web.xml 파일로부터 프로퍼티 파일 이름과 경로 정보 읽어옴
 			String propertiesPath = sc.getRealPath(sc.getInitParameter("contextConfigLocation"));
-			applicationContext = new ApplicationContext(propertiesPath);
+			//applicationContext = new ApplicationContext(propertiesPath);
+			//프로퍼티 파일 내용에 따라 객체 생성
+			applicationContext.prepareObjectsByProperties(propertiesPath);
+			//@Component 어노테이션이 붙은 클래스 찾아 객체 생성
+			applicationContext.prepareObjectsByAnnotation("");
+			//ApplicationContext 클래스에서 관리하는 객체들 의존 객체 주입
+			applicationContext.injectDependency();
 		} catch(Throwable e) {
 			e.printStackTrace();
 		}
--- web07/src/main/java/spms/servlets/DispatcherServlet.java
+++ web07/src/main/java/spms/servlets/DispatcherServlet.java
@@ -33,8 +33,6 @@ public class DispatcherServlet extends HttpServlet {//서블릿이기 때문에 
 		request.setCharacterEncoding("UTF-8");
 		String servletPath = request.getServletPath(); //서블릿 경로 추출
 		try {
-			//ServletContext 객체 준비
-			//ServletContext sc = this.getServletContext();
 			//ApplicationContext 객체 준비
 			ApplicationContext ctx = ContextLoaderListener.getApplicationContext();
 			
@@ -42,12 +40,7 @@ public class DispatcherServlet extends HttpServlet {//서블릿이기 때문에 
 			HashMap<String,Object> model = new HashMap<String,Object>();
 			//model.put("memberDao", sc.getAttribute("memberDao")); //memberDao 객체는 더이상 Map 객체에 담을 필요 없음.
 			model.put("session", request.getSession()); //로그인 및 로그아웃 페이지 컨트롤러에서 사용할 세션 객체
-			
-			//String pageControllerPath = null;
-			//인터페이스 타입의 참조 변수 선언 - Controller의 구현체 클래스들 객체 주소 저장 위함
-			//Controller pageController = null;
-			//ServletContext 객체에서 페이지 컨트롤러 꺼낼 때 서블릿 URL 사용
-			//Controller pageController = (Controller)sc.getAttribute(servletPath);
+
 			//ApplicationContext 객체에서 페이지 컨트롤러 꺼낼 때 서블릿 URL 사용
 			Controller pageController = (Controller)ctx.getBean(servletPath);
 			//찾지 못한다면 오류 발생시킴
@@ -60,61 +53,6 @@ public class DispatcherServlet extends HttpServlet {//서블릿이기 때문에 
 				prepareRequestData(request, model, (DataBinding)pageController);
 			}
 			
-			//페이지 컨트롤러로 위임
-			//조건문 변경 : 페이지 컨트롤러가 사용할 데이터를 준비하는 부분 외 제거
-			/*if("/member/list.do".equals(servletPath)) {
-				//pageControllerPath = "/member/list";
-				//pageController = new MemberListController();
-			}else
-			if("/member/add.do".equals(servletPath)) {
-				//pageControllerPath = "/member/add";
-				//pageController = new MemberAddController();
-				if(request.getParameter("email") != null) {
-					//요청 매개변수로부터 VO 객체 준비
-					//request.setAttribute("member", new Member().setEmail(request.getParameter("email"))
-															   //.setPassword(request.getParameter("password"))
-															   //.setName(request.getParameter("name")));
-					//Map 객체에 VO 객체 준비
-					model.put("member", new Member().setEmail(request.getParameter("email"))
-													.setPassword(request.getParameter("password"))
-													.setName(request.getParameter("name")));
-				}
-			}else if("/member/update.do".equals(servletPath)) {
-				//pageControllerPath = "/member/update";
-				//pageController = new MemberUpdateController();
-				if(request.getParameter("email") != null) {
-					//요청 매개변수로부터 VO 객체 준비
-					//request.setAttribute("member", new Member().setNo(Integer.parseInt(request.getParameter("no")))
-															   //.setEmail(request.getParameter("email"))
-															   //.setName(request.getParameter("name")));
-					//Map 객체에 VO 객체 준비
-					model.put("member", new Member().setNo(Integer.parseInt(request.getParameter("no")))
-													.setEmail(request.getParameter("email"))
-													.setName(request.getParameter("name")));
-				}else {
-					model.put("no", new Integer(request.getParameter("no")));
-		        }
-			}else if("/member/delete.do".equals(servletPath)) {
-				//pageControllerPath = "/member/delete";
-				//pageController = new MemberDeleteController();
-				model.put("no", new Integer(request.getParameter("no")));
-			}else if("/auth/login.do".equals(servletPath)) {
-				//pageControllerPath = "/auth/login";
-				//pageController = new LogInController();
-				if(request.getParameter("email") != null) {
-					model.put("loginInfo", new Member().setEmail(request.getParameter("email"))
-													   .setPassword(request.getParameter("password")));
-				}
-			}
-			else if("/auth/logout.do".equals(servletPath)) {
-				//pageControllerPath = "/auth/logout";
-				//pageController = new LogOutController();
-			}*/
-			
-			//페이지 컨트롤러로 실행 위임
-			/*RequestDispatcher rd = request.getRequestDispatcher(pageControllerPath);
-			rd.include(request, response);*/
-			
 			//페이지 컨트롤러 실행
 			String viewUrl = pageController.execute(model); //뷰 URL 반환받음
 			
@@ -123,15 +61,6 @@ public class DispatcherServlet extends HttpServlet {//서블릿이기 때문에 
 				request.setAttribute(key, model.get(key)); //model.get(key) : value값 가져옴
 			}
 
-			//JSP로 위임
-			/*String viewUrl = (String)request.getAttribute("viewUrl");
-			if(viewUrl.startsWith("redirect:")) {
-				response.sendRedirect(viewUrl.substring(9));
-				return;
-			}else {
-				rd = request.getRequestDispatcher(viewUrl);
-				rd.include(request, response);
-			}*/
 			//JSP로 실행 위임
 			if(viewUrl.startsWith("redirect:")) {
 				response.sendRedirect(viewUrl.substring(9));
