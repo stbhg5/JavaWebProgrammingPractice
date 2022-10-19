53a6596c760bae5bf56a0c4348a58e2b9b779f2b
stbhg5 <stbhg5@gmail.com>
2022-10-19 23:36:07 +0900

    2022.10.19 교재실습

---
+++ lib/src/main/java/exam/test04/Test.java
@@ -0,0 +1,39 @@
+package exam.test04;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		//IoC 컨테이너 준비하여 빈 생성
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("exam/test03/beans.xml");
+		
+		System.out.println("[컨테이너에 보관된 객체의 이름 출력]");
+		for(String name : ctx.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
+		
+		System.out.println("[exam.test03.Score#0의 별명 출력]");
+		for(String alias : ctx.getAliases("exam.test03.Score#0")) {
+			System.out.println(alias);
+		}
+		
+		System.out.println("[exam.test03.Score#1의 별명 출력]");
+		for(String alias1 : ctx.getAliases("exam.test03.Score#1")) {//별명 없음
+			System.out.println(alias1);
+		}
+		
+		System.out.println("[익명 빈 꺼내기]");
+		Score score1 = (Score)ctx.getBean("exam.test03.Score");
+		Score score2 = (Score)ctx.getBean("exam.test03.Score#0");
+		if(score1 == score2) System.out.println("score == score#0");
+		
+		System.out.println("[두 번째로 선언된 익명 빈과 비교]");
+		Score score3 = (Score)ctx.getBean("exam.test03.Score#1");
+		if(score1 != score3) System.out.println("score != score#1");
+		
+		System.out.println("[클래스 타입으로 빈 꺼내기]");
+		Score score4 = (Score)ctx.getBean(exam.test03.Score.class);
+	}
+	
+}
