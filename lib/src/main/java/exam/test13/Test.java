38f38b7755aac6e0f0fca899c746a61fac589215
stbhg5 <stbhg5@gmail.com>
2022-11-06 23:37:51 +0900

    2022.11.06 교재실습

---
+++ lib/src/main/java/exam/test13/Test.java
@@ -0,0 +1,30 @@
+package exam.test13;
+
+import java.util.Map.Entry;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		//IoC 컨테이너 준비하여 빈 생성
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("exam/test13/beans.xml");
+		
+		Tire t1 = (Tire)ctx.getBean("hankookTire");
+		Tire t2 = (Tire)ctx.getBean("kumhoTire");
+		Tire t3 = (Tire)ctx.getBean("hankookTire");
+		
+		System.out.println("t1-->" + t1.toString());
+		System.out.println("t2-->" + t2.toString());
+		System.out.println("t3-->" + t3.toString());
+		
+		if(t1 != t2) {
+			System.out.println("t1 != t2");
+		}
+		
+		if(t1 == t3) {
+			System.out.println("t1 == t3");
+		}
+	}
+	
+}
