fa1d608b1e0d4029ecaab06f09e144ad0ecb66c1
stbhg5 <stbhg5@gmail.com>
2022-11-10 23:25:53 +0900

    2022.11.10 교재실습

---
+++ lib/src/main/java/exam/test19/CustomPropertyEditorRegistrar.java
@@ -0,0 +1,28 @@
+package exam.test19;
+
+import org.springframework.beans.PropertyEditorRegistrar;
+import org.springframework.beans.PropertyEditorRegistry;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+
+public class CustomPropertyEditorRegistrar implements PropertyEditorRegistrar {
+	
+	CustomDateEditor customDateEditor;
+	
+	/**
+	 * 셋터
+	 * @param customDateEditor
+	 */
+	public void setCustomDateEditor(CustomDateEditor customDateEditor) {
+		this.customDateEditor = customDateEditor;
+	}
+
+	/**
+	 * 프로퍼티 에디터 등록
+	 * @param registry
+	 */
+	@Override
+	public void registerCustomEditors(PropertyEditorRegistry registry) {
+		registry.registerCustomEditor(java.util.Date.class, customDateEditor);
+	}
+	
+}
---
+++ lib/src/main/java/exam/test19/Test.java
@@ -0,0 +1,15 @@
+package exam.test19;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		//IoC 컨테이너 준비하여 빈 생성
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("exam/test19/beans.xml");
+		
+		Car car1 = (Car)ctx.getBean("car1");
+		System.out.println(car1.getEngine());
+	}
+	
+}
